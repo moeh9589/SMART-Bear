4e271dd8cac2fe950d4cccdddbd45a2fe1983f44
john-minney-iii <jminneyiii@gmail.com>
2021-11-23 10:30:27 -0700

    Added ChatRoom ID
    This might change. I'm not on backend so I don't know how they are wanting to store these things. I'm just guessing.

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -53,11 +53,11 @@ Future<List<ChatRoom>?> getChatRooms() async {
     QuerySnapshot _chatRoomSnapshot = await _chatRoomCollectionRef.get();
     final _data = _chatRoomSnapshot.docs;
     var _chatRoomList =
-        List.filled(0, ChatRoom(userIds: [], isOpen: false), growable: true);
+        List.filled(0, ChatRoom(id: '', userIds: [], isOpen: false), growable: true);
     // Convert Users from json to local object
     for (var room in _data) {
       _chatRoomList.add(
-          ChatRoom(userIds: List.from(room['Users']), isOpen: room['IsOpen']));
+          ChatRoom(id: room['Id'], userIds: List.from(room['Users']), isOpen: room['IsOpen']));
     }
     return _chatRoomList;
   }
@@ -71,7 +71,7 @@ Future<ChatRoom?> getChatRoomByUsers(String id, String id2) async {
     for (var room in _data) {
       List<String> userIds = [room['Users'][0], room['Users'][1]];
       if (userIds.contains(id) && userIds.contains(id2)) {
-        return ChatRoom(userIds: userIds, isOpen: room['IsOpen']);
+        return ChatRoom(id: room['Id'], userIds: userIds, isOpen: room['IsOpen']);
       }
     }
     return null;
--- lib/models/chatroom.dart
+++ lib/models/chatroom.dart
@@ -1,15 +1,17 @@
 class ChatRoom {
+  String id;
   List<String> userIds;
   bool isOpen;
 
-  ChatRoom({required this.userIds, required this.isOpen});
+  ChatRoom({required this.id, required this.userIds, required this.isOpen});
 
   factory ChatRoom.fromJson(Map<String, dynamic> json) {
-    return ChatRoom(userIds: List.from(json['Users']), isOpen: json['IsOpen']);
+    return ChatRoom(id: json['Id'], userIds: List.from(json['Users']), isOpen: json['IsOpen']);
   }
 
   Map<String, dynamic> getJson() {
     return {
+      'Id': id,
       'Users': [userIds[0], userIds[1]],
       'IsOpen': isOpen
     };

320cae990b7afd3e3a18c20f4af232c50dc703a6
john-minney-iii <jminneyiii@gmail.com>
2021-11-23 18:27:01 -0700

    Student Dashboard View Basic Links

--- lib/views/student_dashboard_view.dart
+++ lib/views/student_dashboard_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smart_bear_tutor/widgets/global_app_bar.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class StudentDashboardView extends StatefulWidget {
   const StudentDashboardView({Key? key}) : super(key: key);
@@ -13,7 +14,73 @@ class _StudentDashboardViewState extends State<StudentDashboardView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: globalAppBar(context, 'Student Dashboard', false, true));
+      backgroundColor: Colors.white,
+      appBar: globalAppBar(context, 'Student Dashboard', false, true),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextButton(
+              child: const Text('Appointments'),
+              onPressed: _appointmentsOnPressed,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextButton(
+              child: const Text('Tutor Availability'),
+              onPressed: _tutorAvailabilityOnPressed,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextButton(
+              child: const Text('Ask a Question'),
+              onPressed: _askQuestionOnPressed,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextButton(
+              child: const Text('FAQ'),
+              onPressed: _faqOnPressed,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: TextButton(
+              child: const Text('Settings'),
+              onPressed: _settingsOnPressed,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _appointmentsOnPressed() async {
+    // redirect user to web page
+    const _url = 'https://www.unco.edu/tutoring/#tutortrac';
+    if (await canLaunch(_url)) {
+      await launch(_url, forceSafariVC: false);
+    } else {
+      throw 'Could Not Launch $_url';
+    }
+  }
+
+  void _tutorAvailabilityOnPressed() {
+    // TODO: move to tutor availability web page
+  }
+
+  void _askQuestionOnPressed() {
+    // TODO: ask a question
+  }
+
+  void _faqOnPressed() {
+    // TODO: move to FAQ view
+  }
+
+  void _settingsOnPressed() {
+    // TODO: move to settings view
   }
 }

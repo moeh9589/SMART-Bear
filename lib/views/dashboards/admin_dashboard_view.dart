d7a3daa6bdb977b322b0323b64d7bbd3105ea7ed
john-minney-iii <jminneyiii@gmail.com>
2021-11-23 19:03:58 -0700

    Admin Dashboard Links

--- lib/views/dashboards/admin_dashboard_view.dart
+++ lib/views/dashboards/admin_dashboard_view.dart
@@ -14,6 +14,76 @@ class _AdminDashboardViewState extends State<AdminDashboardView> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: globalAppBar(context, 'Admin Dashboard', false, true));
+        appBar: globalAppBar(context, 'Admin Dashboard', false, true),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Manage Questions'),
+                onPressed: _manageQuestionsOnPressed,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Manage Tutor Availability'),
+                onPressed: _manageTutorAvailability,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Manage Users'),
+                onPressed: _manageUsersOnPressed,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Manage FAQ'),
+                onPressed: _manageFaqOnPressed,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Send Notification'),
+                onPressed: _sendNotificationsOnPressed,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: TextButton(
+                child: const Text('Settings'),
+                onPressed: _settingsOnPressed,
+              ),
+            )
+          ],
+        ));
+  }
+
+  void _manageQuestionsOnPressed() {
+    // TODO: manage questions view
+  }
+
+  void _manageTutorAvailability() {
+    // TODO: manage tutor availability
+  }
+
+  void _manageUsersOnPressed() {
+    // TODO: manage users
+  }
+
+  void _manageFaqOnPressed() {
+    // TODO: manage faq
+  }
+
+  void _sendNotificationsOnPressed() {
+    // TODO: send notifications
+  }
+
+  void _settingsOnPressed() {
+    // TODO: settings view
   }
 }
